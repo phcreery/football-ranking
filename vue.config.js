edc9ab3b6d2ee653f1080203ae140b601487d8c9
phcreery <phcreery@gmail.com>
2021-12-11 10:24:41 -0600

    update vue base url

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/football-ranking/'
+    : '/',
   configureWebpack: {
     // Merged into the final Webpack config
     module: {
