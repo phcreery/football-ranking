5ec3aad127bce84d3a9896a26ab30a2de87fbf96
phcreery <phcreery@gmail.com>
2021-12-11 20:18:04 -0600

    add dist

--- src/components/API_football.js
+++ src/components/API_football.js
@@ -1,13 +1,20 @@
 async function fetchGames() {
+  let proxy = 'https://thingproxy.freeboard.io/fetch/'
+  // let proxy = 'http://alloworigin.com/get?url='
+  // let proxy = 'https://crossorigin.me/'
+  let request = 'https://api.collegefootballdata.com/games?year=2021&seasonType=regular'
+  // let request = 'https://peyton.creery.org/cfb/games?year=2021&seasonType=regular'
   const response = await fetch(
-    "https://thingproxy.freeboard.io/fetch/https://api.collegefootballdata.com/games?year=2021&seasonType=regular",
+    // request,
+    proxy + request,
+    // 'http://www.whateverorigin.org/get?url=' + encodeURIComponent(request) + '&callback=?',
+    // 'https://api.allorigins.win/raw?url='+ encodeURIComponent(request),
     {
       headers: {
-        accept: "application/json",
+        accept: 'application/json',
         // 'Content-Type': 'application/json',
         // "accept-language": "en-US,en;q=0.9",
-        authorization:
-          "Bearer NJRgktQC/FilkL37+s4ZSvckBKL/Iox5fw18DumOF+X8u2DntZqcJ44P9imkoG+t",
+        authorization: 'Bearer NJRgktQC/FilkL37+s4ZSvckBKL/Iox5fw18DumOF+X8u2DntZqcJ44P9imkoG+t',
         // "if-none-match": "W/\"93f97-bd8lO4nIKIW0ZF6n4neQI+4nSe8\"",
         // "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"96\", \"Google Chrome\";v=\"96\"",
         // "sec-ch-ua-mobile": "?0",
@@ -15,20 +22,20 @@ async function fetchGames() {
         // "sec-fetch-dest": "empty",
         // "sec-fetch-mode": "cors",
         // "sec-fetch-site": "same-origin",
-        // "Origin": "bridged.xyz",
+        "Origin": "localhost",
         // 'Access-Control-Allow-Origin': '*'
       },
       // "referrerPolicy": "no-referrer",
       body: null,
-      method: "GET",
-      mode: "cors",
+      method: 'GET',
+      mode: 'cors',
       // "credentials": "include",
       // "crossDomain": true,
     }
-  );
-  let json = await response.json();
-  console.log("res", json);
-  return json;
+  )
+  let json = await response.json()
+  console.log('res', json)
+  return json
 }
 
-export { fetchGames };
+export { fetchGames }
